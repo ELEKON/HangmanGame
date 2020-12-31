350b95d6755078cff10a0e20e4bd862cf3643e5f
ELEKON <konelefas@hotmail.com>
2020-12-31 12:26:56 +0200

    Second try

--- PlayHangman.java
+++ PlayHangman.java
@@ -4,7 +4,7 @@ public class PlayHangman {
 	public static void main(String[] args) throws Exception { 
 	  
 		//wait for calling the right methods
-		
+		System.out.println("Git dokimh");
 		HangmanGameEntity entity = new HangmanGameEntity();
 		System.out.println(entity.chooseFromExistingWords());
 		
