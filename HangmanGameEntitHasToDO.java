7ca6aa48d74aea15d9187f4b5977b80766eb20e0
ELEKON <konelefas@hotmail.com>
2020-12-31 11:26:10 +0200

    Initial commit with GitBash

---
+++ HangmanGameEntitHasToDO.java
@@ -0,0 +1,6 @@
+
+public interface HangmanGameEntitHasToDO {
+	public String chooseFromExistingWords() throws Exception;
+	public void StartNewGame() throws Exception;
+	public boolean WinningMessage();
+}
---
+++ PlayHangman.java
@@ -0,0 +1,13 @@
+public class PlayHangman {
+
+	
+	public static void main(String[] args) throws Exception { 
+	  
+		//wait for calling the right methods
+		
+		HangmanGameEntity entity = new HangmanGameEntity();
+		System.out.println(entity.chooseFromExistingWords());
+		
+	}
+	
+}	
